0b566d2e99f65c3908a146afe36834e0966638ad
Ajaychintala65 <ajaychintala6655@gmail.com>
2023-04-02 14:07:06 -0500

    adding the app_data into the project

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CabReservationSystem
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "CabReservation", action = "Login", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

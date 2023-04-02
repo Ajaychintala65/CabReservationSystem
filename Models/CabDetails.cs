8e1525a030b4e03bd9f3943912da45077ab9ced6
AbdulYaser <mohammedyaser1521@gmail.com>
2023-04-02 14:20:05 -0500

    pushing the code files into the repository

---
+++ Models/CabDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CabReservationSystem.Models
+{
+    public class CabDetails
+    {
+        public string ID { get; set; }
+        public string ImagePath { get; set; }
+        public string VehicleName { get; set; }
+        public string Description { get; set; }
+    }
+}
